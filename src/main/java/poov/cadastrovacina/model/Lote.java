a02c1d1aced623c70be4f5a8595fa2abf214b202
FulvioDiniz <fulviodiniz0@gmail.com>
2023-05-30 10:38:30 -0300

    Criação do lote e vacina

---
+++ src/main/java/poov/cadastrovacina/model/Lote.java
@@ -0,0 +1,12 @@
+package poov.cadastrovacina.model;
+
+import java.time.LocalDate;
+
+public class Lote {
+    private Long codigo;
+    private LocalDate Validade;
+    private int nroDosesAtual;
+    private int nroDosesLote;
+    private Vacina vacina;
+    
+}
